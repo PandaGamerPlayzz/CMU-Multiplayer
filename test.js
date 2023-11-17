6ab2672479962b546cd360cf8521990e9212d3d3
PandaGamerPlayzz <zacharymiller515@gmail.com>
2023-11-16 20:54:52 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,22 +1,30 @@
-console.log("HELLO WORLD BABY!")
+// console.log("HELLO WORLD BABY!");
 
-const body = document.getElementsByTagName('body')[0];
+var $module = (() => {
+  function showAmogus() {
+    const body = document.getElementsByTagName('body')[0];
 
-const amogus_div = document.createElement('div');
-amogus_div.style.backgroundColor = 'rgb(30, 30, 30)';
-amogus_div.style.display = 'flex';
-amogus_div.style.alignItems = 'center';
-amogus_div.style.justifyContent = 'center';
-amogus_div.style.margin = '0';
-amogus_div.style.position = 'absolute';
-amogus_div.style.left = '0';
-amogus_div.style.top = '0';
-amogus_div.style.width = '100vw';
-amogus_div.style.height = '100vh';
-amogus_div.style.zIndex = Number.MAX_SAFE_INTEGER;
+    const amogus_div = document.createElement('div');
+    amogus_div.style.backgroundColor = 'rgb(30, 30, 30)';
+    amogus_div.style.display = 'flex';
+    amogus_div.style.alignItems = 'center';
+    amogus_div.style.justifyContent = 'center';
+    amogus_div.style.margin = '0';
+    amogus_div.style.position = 'absolute';
+    amogus_div.style.left = '0';
+    amogus_div.style.top = '0';
+    amogus_div.style.width = '100vw';
+    amogus_div.style.height = '100vh';
+    amogus_div.style.zIndex = Number.MAX_SAFE_INTEGER;
 
-const amogus_img = document.createElement('img');
-amogus_img.src = 'https://media.tenor.com/dEpZxRPM2D0AAAAC/booty-among-us.gif'
-amogus_div.appendChild(amogus_img);
+    const amogus_img = document.createElement('img');
+    amogus_img.src = 'https://media.tenor.com/dEpZxRPM2D0AAAAC/booty-among-us.gif'
+    amogus_div.appendChild(amogus_img);
 
-body.appendChild(amogus_div);
+    body.appendChild(amogus_div);
+  }
+
+  return {
+    showAmogus: showAmogus
+  }
+});
